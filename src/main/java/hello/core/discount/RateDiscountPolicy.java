42ac62deaa47d63ce366602d34f9141650e19991
dodeon <sojeongw.official@gmail.com>
2021-03-23 06:23:10 +0900

    스프링 핵심 원리 이해: 객체 지향 원리 적용 - 새로운 할인 정책 적용과 문제점

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,2 +1,18 @@
-package hello.core.discount;public class RateDiscountPolicy {
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+  private int discountPercent = 10;
+
+  @Override
+  public int discount(Member member, int itemPrice) {
+    if(member.getGrade() == Grade.VIP) {
+      return itemPrice * discountPercent / 100;
+    } else {
+      return 0;
+    }
+  }
 }
