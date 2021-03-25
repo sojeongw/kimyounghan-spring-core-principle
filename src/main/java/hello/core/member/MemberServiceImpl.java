79ba6ad72eb3787f91bc65655ca74a657af57403
dodeon <sojeongw.official@gmail.com>
2021-03-26 07:37:12 +0900

    스프링 핵심 원리 이해: 객체 지향 원리 적용 - 관심사의 분리

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -4,7 +4,11 @@ public class MemberServiceImpl implements MemberService {
 
   // 인터페이스만 가지고 있으면 구현체가 없어서 NPE가 터진다.
   // 따라서 구현 객체(MemoryMemberRepository)를 선택해줘야 한다.
-  private final MemberRepository memberRepository = new MemoryMemberRepository();
+  private final MemberRepository memberRepository;
+
+  public MemberServiceImpl(MemberRepository memberRepository) {
+    this.memberRepository = memberRepository;
+  }
 
   @Override
   public void join(Member member) {
