5293849f2737c4d4ca11a74baf5f18b5cacda960
dodeon <sojeongw.official@gmail.com>
2021-03-29 12:17:04 +0900

    스프링 핵심 원리 이해: 객체 지향 원리 적용 - 객체 지향 설계의 5가지 원칙 적용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -8,12 +10,25 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
+  // 메서드 명을 보는 순간 역할이 드러난다.
+  // memberService에서는 memberServiceImpl을 쓸 것이다.
   public MemberService memberService() {
-    return new MemberServiceImpl(new MemoryMemberRepository());
+    return new MemberServiceImpl(memberRepository());
+  }
+
+  // memberRepository는 memory로 쓸 것이다.
+  // 그래서 나중에 다른 repository를 쓰려면 이 코드만 바꾸면 된다.
+  private MemberRepository memberRepository() {
+    return new MemoryMemberRepository();
   }
 
   public OrderService orderService() {
-    return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    return new OrderServiceImpl(memberRepository(), discountPolicy());
+  }
+
+  private DiscountPolicy discountPolicy() {
+//    return new FixDiscountPolicy();
+    return new RateDiscountPolicy();
   }
 
 }
