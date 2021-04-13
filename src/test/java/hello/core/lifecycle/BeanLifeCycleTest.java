8afd5be530f9106d8a30c33d291028614e727d4c
dodeon <sojeongw.official@gmail.com>
2021-04-13 09:24:15 +0900

    스프링 핵심 원리 이해: 빈 생명 주기 콜백 - 인터페이스 방식

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -6,11 +6,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-public class BeanLifeCycleTest {
+public class BeanLifeCycleTest{
 
   @Test
   public void lifeCycleTest() {
-    ConfigurableApplicationContext ac =  new AnnotationConfigApplicationContext(NetworkClient.class);
+    ConfigurableApplicationContext ac =  new AnnotationConfigApplicationContext(LifeCycleConfig.class);
     NetworkClient client = ac.getBean(NetworkClient.class);
 
     // ApplicatoinContext 대신, 이를 상속하는
--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
   private String url;
 
   public NetworkClient() {
     System.out.println("생성자 호출, url = " + url);
-    connect();
-    call("초기화 연결 메시지");
   }
 
   public void setUrl(String url) {
@@ -27,5 +28,20 @@ public class NetworkClient {
   // 서비스 종료 시 호출하는 메서드
   public void disconnect() {
     System.out.println("closed url = " + url);
+  }
+
+  // InitializingBean
+  // 프로퍼티가 세팅이 끝나면 = 의존 관게 주입이 끝나면 호출한다.
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    connect();
+    call("초기화 연결 메시지");
+  }
+
+  // DisposableBean
+  // 빈이 종료될 때 호출된다.
+  @Override
+  public void destroy() throws Exception {
+    disconnect();
   }
 }
