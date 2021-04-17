4262369ababf6caa6b8a4e43855dc7fc5a385b7c
dodeon <sojeongw.official@gmail.com>
2021-04-17 15:55:54 +0900

    스프링 핵심 원리 이해: 프로토타입 스코프 - 스코프와 프록시

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -3,7 +3,6 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,15 +13,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
   private final LogDemoService logDemoService;
-  // myLogger를 찾을 수 있는, DL할 수 있는 객체가 주입되어 주입 시점에 myLogger을 주입받을 수 있다.
-  private final ObjectProvider<MyLogger> myLoggerProvider;
+  private final MyLogger myLogger;
 
   @RequestMapping("log-demo")
   @ResponseBody
   public String logDemo(HttpServletRequest request) {
-    // DL로 원하는 객체를 가져온다.
-    MyLogger myLogger = myLoggerProvider.getObject();
-
+    System.out.println("myLogger = " + myLogger.getClass());
     String requestURL = request.getRequestURL().toString();
     myLogger.setRequestURL(requestURL);
 
